fab14386a70fa95d9f2e216d93abf9afec93f0a8
Gabriel Inacio <inaciogs03@gmail.com>
2021-08-12 09:46:55 -0300

    atualizado

--- script.php
+++ script.php
@@ -17,42 +17,46 @@
 	if(empty($vNome) || empty($vIdade)){
 		$_SESSION['erro'] = '<br>Todos os campos devem ser preenchidos';		
 		header('Location:index.php');
-	}if(strlen($vNome) < 3 || strlen($vNome) > 32){
+		return;
+	}else if(strlen($vNome) < 3 || strlen($vNome) > 32){
 		$_SESSION['erro'] = '<br>O nome deve conter mais de 3 caracteres';
-		header('Location:index.php');				
-	}if($vIdade < 6){
+		header('Location:index.php');	
+		return;			
+	}else if($vIdade < 6){
 		$_SESSION['erro'] = '<br>A idade minima é 6';	
-		header('Location:index.php');			
-	}if(!is_numeric($vIdade)){
+		header('Location:index.php');	
+		return;		
+	}else if(!is_numeric($vIdade)){
 		$_SESSION['erro'] = '<br>A idade tem que ser um valor inteiro';		
-		header('Location:index.php');		
+		header('Location:index.php');	
+		return;	
 	}
-		//condições
-		if($vIdade >= 6 && $vIdade <= 12){
-			for ($i=0; $i < count($categoria); $i++) { 
-				if($categoria[$i] == "Infantil"){
-					echo "Atleta: ".$vNome."<br>";
-					echo "Idade: ".$vIdade."<br>";
-					echo "Categoria: ".$categoria[$i]."<br>";
-				}
+	//condições
+	if($vIdade >= 6 && $vIdade <= 12){
+		for ($i=0; $i < count($categoria); $i++) { 
+			if($categoria[$i] == "Infantil"){
+				echo "Atleta: ".$vNome."<br>";
+				echo "Idade: ".$vIdade."<br>";
+				echo "Categoria: ".$categoria[$i]."<br>";
 			}
-		} else if($vIdade >12 && $vIdade <= 18){
-			for ($i=0; $i < count($categoria); $i++) { 
-				if($categoria[$i] == "Adolescente"){
-					echo "Atleta: ".$vNome."<br>";
-					echo "Idade: ".$vIdade."<br>";
-					echo "Categoria: ".$categoria[$i]."<br>";
-				}
+		}
+	} else if($vIdade >12 && $vIdade <= 18){
+		for ($i=0; $i < count($categoria); $i++) { 
+			if($categoria[$i] == "Adolescente"){
+				echo "Atleta: ".$vNome."<br>";
+				echo "Idade: ".$vIdade."<br>";
+				echo "Categoria: ".$categoria[$i]."<br>";
 			}
-		} else {
-			for ($i=0; $i < count($categoria); $i++) { 
-				if($categoria[$i] == "Adulto"){
-					echo "Atleta: ".$vNome."<br>";
-					echo "Idade: ".$vIdade."<br>";
-					echo "Categoria: ".$categoria[$i]."<br>";
-				}
+		}
+	} else {
+		for ($i=0; $i < count($categoria); $i++) { 
+			if($categoria[$i] == "Adulto"){
+				echo "Atleta: ".$vNome."<br>";
+				echo "Idade: ".$vIdade."<br>";
+				echo "Categoria: ".$categoria[$i]."<br>";
 			}
-		}		
-	
+		}
+	}		
+
 	
 ?>
